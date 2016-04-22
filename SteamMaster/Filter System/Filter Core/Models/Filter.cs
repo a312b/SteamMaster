34e56991acc22f49a2c3146aa0b30b8685ecc916
Hoeiriis <jeppe.h.m@hotmail.com>
2016-04-22 17:57:22 +0200

    Filter SuperClass tilføjet, mere implementeres senere

---
+++ SteamMaster/Filter System/Filter Core/Models/Filter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter_System.Filter_Core.Models
+{
+    abstract class Filter <T>
+    {
+        protected Filter()
+        {
+            FilterWeight = 1;
+        }
+
+
+        public virtual Dictionary<int, double> Execute(Dictionary<int, T> dictionaryToSort) // Execute FilterSort and applies weight
+        {
+            Dictionary<int, double> returnDictionary = FilterSort(dictionaryToSort);
+
+            List<int> keys = new List<int>(returnDictionary.Keys);
+            foreach (int key in keys)
+            {
+                double assignValue;
+                returnDictionary.TryGetValue(key, out assignValue);
+
+                returnDictionary[key] = assignValue*FilterWeight;
+            }
+
+            return returnDictionary;
+        }
+
+
+        protected abstract Dictionary<int, double> FilterSort(Dictionary<int, T> dictionaryToSort);
+
+        // Takes a dictionary with SortParameters related to appID -> dictionaryToSort<appID, sortParameter>
+        // Returns a dictionary with value related to appID - the appID with the highest value is most recommended
+        // Inheriting class implement the sort rule and type of sort parameter
+        // Is used in Execute
+
+        public double FilterWeight // Used to adjust weight of the filter by setting the value between 0-1 - default is 1
+        {
+            get { return FilterWeight; }
+            set
+            {
+                if(value >= 0 && value <= 1)
+                {
+                    FilterWeight = value;
+                } else
+                {
+                    throw new ArgumentException("Weight have to be between 0 and 1");
+                }
+            }
+        }
+    }
+}
+
---
+++ SteamMaster/Filter System/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<int, string> woop = new Dictionary<int, string>();
+
+            woop.Add(1, "Hej");
+            woop.Add(2, "Dig");
+
+            woop[2] = "Loooooow rider";
+
+            Console.WriteLine($"{woop[2]}");
+
+            Console.ReadKey();
+        }
+    }
+}
